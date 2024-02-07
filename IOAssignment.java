242013905dac16e23413ba4c8344af70a75e8a76
Mauricio Lopez Alvarez <mauriciolopezalvarez@Mauricios-MBP.attlocal.net>
2024-02-07 10:29:44 -0500

    First commit for IOAsignment

---
+++ IOAssignment.java
@@ -0,0 +1,17 @@
+/*
+ *
+ */
+package cop2805;
+
+import java.io.*;
+import java.util.*;
+
+
+public class IOAssignment 
+{
+    public static void main(String[] args) 
+    {
+        
+    } // end main
+    
+} // end IOAssignment class
